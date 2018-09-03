f3715defce878ad63ea9344af6c0b075f25cd859
sushmita kumari <sushmita2607k@gmail.com>
2018-09-03 13:05:02 +0530

    p1.add numbers

---
+++ src/AddNumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class AddNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int num;
+		while(sc.hasNext()) {
+			System.out.println(sc.hasNextInt());
+		}
+	}
+
+}
